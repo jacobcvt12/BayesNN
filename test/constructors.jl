9e2b7b5d285a7e755748f7dccff661541ea98779
Jacob Carey <JCarey@telligen.com>
2017-12-21 10:45:01 -0500

    Add a test for the constructor

---
+++ test/constructors.jl
@@ -0,0 +1,17 @@
+@testset "Constructors" begin
+    using Distributions, BayesNN, StatsFuns
+
+    # data size
+    N = 1000  # number of observations
+    D = 3     # number of covariates including intercept
+
+    # generate data
+    srand(1)                  # insure reproducibility
+    X = rand(Normal(), N, D)  # generate covariates
+    X[:, 1] = 1               # make first column intercept
+    b = rand(Normal(), D)     # coefficients
+    θ = logistic.(X * b)      # simulate dependent variables
+    y = rand.(Bernoulli.(θ))  # simulate dependent variables
+
+    @test_nowarn net = Nnet(y, X)
+end
